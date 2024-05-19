013ce71e2552cb744fab4c02b6275516738f6fee
Ajay Kaloty <kalotya99@gmail.com>
2024-05-18 20:50:38 -0700

    init function

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -7,8 +7,22 @@ function init () {
     renderHistory();
 
     $("#search-button").on('click', function(){
-        var city = $();
-    })
+        var city = $("#input-city").val();
+        
+        if (city === "") {
+            window.alert("There is no city provided! Please provide a city.");
+            return;
+        }
+
+        renderWeather(city);
+
+        if (state.history.indexOf(city) === -1) {
+            state.history.push(city);
+            saveState()
+        }
+
+        renderHistory();
+    });
 };
 
 function loadState() {};
