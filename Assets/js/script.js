0b52242c051d7b420ac430a2d2ea9ee7c12f84cd
Ajay Kaloty <kalotya99@gmail.com>
2024-05-18 20:17:58 -0700

    Added skeleton functions

---
+++ Assets/js/script.js
@@ -0,0 +1,15 @@
+var state = {
+    history: []
+};
+
+function init () {};
+
+function loadState() {};
+
+function saveState() {};
+
+function renderWeather() {};
+
+function renderHistory() {};
+
+init();
